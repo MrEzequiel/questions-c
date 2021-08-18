02f72bf5db51d3ff79e45f94cc4c24239aa13703
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2021-08-18 18:35:22 -0500

    question2

---
+++ src/question_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+  system("cls");
+  setlocale(LC_ALL, "Portuguese");
+
+  double area = 0, pi = 3.14, raio = 0;
+
+  printf("\n\nEste programa calcula a area de um circulo!");
+  printf("\n\nDigite o valor do raio do circulo: ");
+  scanf("%d", &raio);
+
+  area = pi * raio * raio;
+
+  printf("\n\nA area do circulo é ", area);
+}
